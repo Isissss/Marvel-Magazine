7e0129d325974f06f553387e4dc01b30a2667cdf
Administrator <1036029@hr.nl>
2022-03-19 02:23:02 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,35 +1,57 @@
 window.addEventListener('load', init);
 
+// class Movie {
+//     constructor(id, name, cover) {
+//         this.id = id;
+//         this.name = name;
+//         this.cover = cover;
+//     }
+//
+// }
 //Global vars
 let element = document.body
+let grid = document.querySelector('.grid');
+let details = document.querySelector('.movie-info');
+
 /**
  * Execute after document is fully loaded
  */
 function init() {
-   let button = document.getElementById('switch');
+    let button = document.getElementById('switch');
     button.addEventListener('click', darkModeToggle);
 
-    if (localStorage.getItem('isDarkMode') === 'true') {
-        element.classList.add('active-dark');
-    }
-
+    grid.addEventListener("click", gridClickHandler)
 }
 
 const darkModeToggle = () => {
     element.classList.toggle("dark-mode");
 }
 
-const favebtn = document.querySelector(".fav-btn");
-const hello = document.querySelector('.movie-name');
+function gridClickHandler(e) {
+    let target = e.target;
+    let movieName = target.parentNode.querySelector(".movie-name > h3");
 
-favebtn.addEventListener("click", e => {
-    if (favebtn.classList.contains("active")) {
-        favebtn.classList.remove("active");
-        hello.classList.remove("active");
-
-    } else {
-        favebtn.classList.add("active");
-        hello.classList.add("active");
-
+    if (target.nodeName === "BUTTON") {
+        if (target.classList.contains("active")) {
+            removeFavorite(target);
+        } else {
+            addFavorite(target)
+        }
+    } else if (target.nodeName === "IMG") {
+    details.innerHTML = movieName.innerHTML
     }
-})
+}
+
+function removeFavorite(target) {
+    target.classList.remove("active");
+    target.parentNode.classList.remove("active");
+
+}
+
+function addFavorite(target) {
+    target.classList.add("active");
+    target.parentNode.classList.add("active");
+}
+
+
+
--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -7,60 +7,102 @@ function getDishes()
     return [
         [
             "id" => 1,
-            "name" => "Pizza",
-            "kitchen" => "Italian",
+            "name" => "Avengers: End Game",
+            "cover" => "https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg?region=0,0,540,810&width=480",
         ],
         [
             "id" => 2,
-            "name" => "Kale",
-            "kitchen" => "Dutch",
+            "name" => "Captain Marvel",
+            "cover" => "https://terrigen-cdn-dev.marvel.com/content/prod/1x/captainmarvel_lob_crd_06.jpg",
         ],
         [
             "id" => 3,
-            "name" => "Lasagna",
-            "kitchen" => "Italian",
+            "name" => "Black Widow",
+            "cover" => "https://m.media-amazon.com/images/M/MV5BNjRmNDI5MjMtMmFhZi00YzcwLWI4ZGItMGI2MjI0N2Q3YmIwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
         ],
         [
             "id" => 4,
-            "name" => "Kebab",
-            "kitchen" => "Turkish",
+            "name" => "Thor: The Dark World",
+            "cover" => "https://static1.srcdn.com/wordpress/wp-content/uploads/2019/06/Thor-The-Dark-World.jpg?q=50&fit=crop&w=963&h=1427&dpr=1.5",
         ],
         [
             "id" => 5,
-            "name" => "Paella",
-            "kitchen" => "Spanish",
+            "name" => "Guardians of the Galaxy",
+            "cover" => "https://m.media-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_.jpg",
+        ],
+        [
+            "id" => 6,
+            "name" => "Black Panther",
+            "cover" => "https://i.pinimg.com/originals/01/7a/8f/017a8f1b5d6cd56f8a66459a2715ba3e.png",
+        ],
+        [
+            "id" => 7,
+            "name" => "Avengers: Infinity War",
+            "cover" => "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg",
         ]
     ];
 }
 
-/**
- * @param $id
- * @return mixed
- */
-function getDishDetails($id)
-{
-    $tags = [
-        1 => [
-            "recipe" => "Put it in the oven and go!",
-            "tags" => ['cheese', 'oven']
-        ],
-        2 => [
-            "recipe" => "You can make this delicious Dutch meal by ...",
-            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
-        ],
-        3 => [
-            "recipe" => "Very nice when your grandma prepares this meal",
-            "tags" => ['omnomnom']
-        ],
-        4 => [
-            "recipe" => "Everytime in the city after midnight",
-            "tags" => ['kapsalon', 'tasty', 'meat']
-        ],
-        5 => [
-            "recipe" => "Specialty when on holiday in Spain",
-            "tags" => ['fish']
-        ],
-    ];
 
-    return $tags[$id];
-}
+//function getDishes()
+//{
+//    return [
+//        [
+//            "id" => 1,
+//            "name" => "Pizza",
+//            "kitchen" => "Italian",
+//        ],
+//        [
+//            "id" => 2,
+//            "name" => "Kale",
+//            "kitchen" => "Dutch",
+//        ],
+//        [
+//            "id" => 3,
+//            "name" => "Lasagna",
+//            "kitchen" => "Italian",
+//        ],
+//        [
+//            "id" => 4,
+//            "name" => "Kebab",
+//            "kitchen" => "Turkish",
+//        ],
+//        [
+//            "id" => 5,
+//            "name" => "Paella",
+//            "kitchen" => "Spanish",
+//        ]
+//    ];
+//}
+//
+///**
+// * @param $id
+// * @return mixed
+// */
+//function getDishDetails($id)
+//{
+//    $tags = [
+//        1 => [
+//            "recipe" => "Put it in the oven and go!",
+//            "tags" => ['cheese', 'oven']
+//        ],
+//        2 => [
+//            "recipe" => "You can make this delicious Dutch meal by ...",
+//            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
+//        ],
+//        3 => [
+//            "recipe" => "Very nice when your grandma prepares this meal",
+//            "tags" => ['omnomnom']
+//        ],
+//        4 => [
+//            "recipe" => "Everytime in the city after midnight",
+//            "tags" => ['kapsalon', 'tasty', 'meat']
+//        ],
+//        5 => [
+//            "recipe" => "Specialty when on holiday in Spain",
+//            "tags" => ['fish']
+//        ],
+//    ];
+//
+//    return $tags[$id];
+//}
