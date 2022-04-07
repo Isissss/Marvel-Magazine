b43d6ecd5ad6c953ebf6e1e3359d30a675dbe37d
Isis <1036029@hr.nl>
2022-04-07 15:24:43 +0200

    Cleaned up code and added modal

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 window.addEventListener('load', init);
 
-// class Movie {
-//     constructor(id, name, cover) {
-//         this.id = id;
-//         this.name = name;
-//         this.cover = cover;
-//     }
-//
-// }
 //Global vars
 let grid
 let details
 let favorites
 let button
-let infoheader
-let apikey = "c196ca2fae8666873c3683d32b8d6cf4"
+let movieName
+let movies = {}
+let detailModal;
+let modalHeader;
+let detailModalContent;
+let detailModalCloseButton;
+let overlay;
+let apiKey = "c196ca2fae8666873c3683d32b8d6cf4"
 let apiUrl = 'http://localhost:63342/Magazinee/webservice/index.php';
 
 /**
@@ -26,29 +24,32 @@ function init() {
 
     favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
-    let lightswitch = document.getElementById('switch');
-    lightswitch.addEventListener('click', lightModeToggle);
-
-    details = document.querySelector('.movie-info');
+    document.getElementById('switch').addEventListener('click', lightModeToggle);
 
     grid = document.querySelector('.grid');
     grid.addEventListener("click", gridClickhandler)
 
-    infoheader = document.querySelector('#movie-name');
+    let topbtn = document.getElementById("topBtn")
+    topbtn.addEventListener("click", goTop);
 
     window.addEventListener("scroll", e => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            topbtn.style.display = "block";
+        } else {
+            topbtn.style.display = "none";
+        }
     })
 
-    let selector = document.querySelector(".filter-movies")
-    selector.addEventListener("click", filterMovies)
+    document.querySelector(".filter-movies").addEventListener("click", filterMovies)
 
-    let mybutton = document.getElementById("myBtn");
-    mybutton.addEventListener("click", goTop)
+    detailModal = document.getElementById('movie-detail');
+    detailModalContent = detailModal.querySelector('.modal-content');
+
+    detailModalCloseButton = detailModal.querySelector('.modal-close');
+    detailModalCloseButton.addEventListener('click', closeModal);
+
+    overlay = document.querySelector('.modal-bg')
+    overlay.addEventListener("click", closeModal)
 }
 
 function checkLightMode() {
@@ -57,10 +58,11 @@ function checkLightMode() {
     }
 }
 
-function  goTop (e) {
+function goTop(e) {
     document.body.scrollTop = 0
     document.documentElement.scrollTop = 0
 }
+
 function getMovieData(url, succesHandler) {
     fetch(url)
         .then((response) => {
@@ -73,18 +75,17 @@ function getMovieData(url, succesHandler) {
         .catch(ajaxErrorHandler);
 }
 
-function gridClickhandler (e) {
+function gridClickhandler(e) {
     let target = e.target;
+    if (target.nodeName !== "BUTTON") {
+        return;
+    }
 
-    if (target.nodeName === "BUTTON") {
-        if (target.classList.contains("active")) {
-            removeFavorite(target);
-        } else {
-            addFavorite(target)
-        }
-    } else if (target.nodeName === "IMG" && target.id !== 'infopic') {
+    if (target.classList.contains("fav-btn")) {
+        let favorite = (target.classList.contains("active")) ? removeFavorite(target) : addFavorite(target);
+    } else if (target.classList.contains("details")) {
         getMovieData(`${apiUrl}?id=${target.dataset.id}&append_to_response=images`, ShowDetails);
-        movieName = target.parentNode.querySelector(".movie-name > h3");
+        movieName = movies[target.dataset.id]['name'];
     }
 }
 
@@ -119,17 +120,30 @@ function createMovieCards(data) {
 
         let image = document.createElement('img');
         image.src = movie.cover;
-        image.dataset.id = movie.id;
 
         movieCard.appendChild(image);
+
+        let info = document.createElement("button")
+        info.innerHTML = "View details"
+        info.classList.add("details");
+        info.dataset.id = movie.id;
+
+        movieCard.appendChild(info);
+
         grid.appendChild(movieCard);
+
+        movies[movie.id] = movie
     }
 }
 
-let checkIfFavorite = id => { if (favorites.indexOf(id) === -1) { return 'true' }}
+let checkIfFavorite = id => {
+    if (favorites.indexOf(id) !== -1) {
+        return 'true'
+    }
+}
 
 
-const lightModeToggle = (e) => {
+let lightModeToggle = e => {
     let lightMode = localStorage.getItem('lightMode');
 
     if (lightMode === "true") {
@@ -142,40 +156,39 @@ const lightModeToggle = (e) => {
 }
 
 function ShowDetails(data) {
-    details.innerHTML = "";
-    infoheader.innerHTML = "";
+    detailModalContent.innerHTML = "";
+
+    let h3 = document.createElement("h1");
+    h3.innerHTML = `${movieName} (${data.year}) `
+    detailModalContent.appendChild(h3);
+
 
     let title = document.createElement('h4')
     title.innerHTML = "Description"
-    details.appendChild(title)
+    detailModalContent.appendChild(title)
 
     let desc = document.createElement("p")
     desc.innerHTML = `${data.desc}`
-    details.appendChild(desc)
+    detailModalContent.appendChild(desc)
 
     let title2 = document.createElement('h4')
     title2.innerHTML = "Tags"
-    details.appendChild(title2)
+    detailModalContent.appendChild(title2)
 
     let tags = document.createElement("p")
     tags.innerHTML = data.tags.join(", ")
-    details.appendChild(tags)
+    detailModalContent.appendChild(tags)
 
-    let h3 = document.createElement("h3");
-    h3.innerHTML = `${movieName.innerHTML} (${data.year}) `
-
-    getMovieData(`https://api.themoviedb.org/3/movie/${data.mdbID}?api_key=${apikey}&append_to_response=images&include_image_language=en,null`, getRating)
-
-    infoheader.appendChild(h3);
+    getMovieData(`https://api.themoviedb.org/3/movie/${data.mdbID}?api_key=${apiKey}&append_to_response=images&include_image_language=en,null`, getRating)
 }
 
-function getRating (data) {
-    let random = ( Math.floor(Math.random() * (data.images.backdrops.length)));
+function getRating(data) {
+    let random = (Math.floor(Math.random() * (data.images.backdrops.length)));
 
     let img = document.createElement("img");
     img.id = "infopic";
     img.src = `https://www.themoviedb.org/t/p/original/${data.images.backdrops[random].file_path}`
-
+    detailModalContent.appendChild(img);
 
     let rating = document.createElement('span');
     rating.innerHTML = `&#9733 ${data.vote_average}`;
@@ -191,16 +204,15 @@ function getRating (data) {
             rating.classList.add("red");
             break;
     }
+    detailModalContent.appendChild(rating);
 
-    details.appendChild(img)
-    infoheader.appendChild(rating);
-
+    detailModal.classList.add('open');
 }
 
 function removeFavorite(target) {
     target.classList.remove("active");
-    target.parentElement.classList.remove("active");
-    target.parentElement.parentElement.classList.remove("fave");
+    target.parentNode.classList.remove("active");
+    target.parentNode.parentElement.classList.remove("fave");
 
     let index = favorites.indexOf(target.dataset.id);
     favorites.splice(index, 1);
@@ -209,8 +221,8 @@ function removeFavorite(target) {
 
 function addFavorite(target) {
     target.classList.add("active");
-    target.parentElement.classList.add("active");
-    target.parentElement.parentElement.classList.add("fave");
+    target.parentNode.classList.add("active");
+    target.parentNode.parentElement.classList.add("fave");
 
     favorites.push(target.dataset.id);
     localStorage.setItem('favorites', JSON.stringify(favorites));
@@ -218,7 +230,7 @@ function addFavorite(target) {
 
 function filterMovies(e) {
     const movies = document.querySelectorAll(".card");
-    console.log(movies);
+
     movies.forEach(movie => {
         switch (e.target.value) {
             case "all":
@@ -234,11 +246,17 @@ function filterMovies(e) {
     });
 }
 
+let closeModal = (e) => {
+    detailModal.classList.remove('open')
+}
+
 
 function ajaxErrorHandler(data) {
+    grid.before.innerHTML = "";
+
     console.log(data);
     let error = document.createElement('div');
     error.classList.add('error');
-    error.innerHTML = "Error :(";
+    error.innerHTML = "Error, check console!";
     grid.before(error);
 }
